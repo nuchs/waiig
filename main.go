b0301e9f9f8ddefc5f913dbafa50897dcd8da423
nuchs <sjbrown@live.co.uk>
2024-01-12 16:40:07 +0000

    Add simple lexing repl

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello monkey")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming langauage!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
+	fmt.Println("\nI love you, bye bye")
 }
